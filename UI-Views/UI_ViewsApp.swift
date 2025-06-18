1d31ddbf223033908bfe40511c099829a214e1c7
Fabricio Padua <fabricio_0505_@hotmail.com>
2025-06-18 17:00:34 -0400

    push project

---
+++ UI-Views/UI_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ViewsApp.swift
+//  UI-Views
+//
+//  Created by Fabricio Padua on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct UI_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
